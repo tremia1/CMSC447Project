77f40f312701f002d751d922849b4cf9bfc1ea1c
wweng2 <99202437+wweng2@users.noreply.github.com>
2023-03-24 14:27:22 -0400

    Update Game.js
    Put the scenes you want to test into scene in the order it will run to make it work

--- src/main/java/com.app/Game.js
+++ src/main/java/com.app/Game.js
@@ -1,1 +1,17 @@
+import Phaser from 'phaser';
 
+export default {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+    },
+  },
+  
+      scene: [
+ 
+    ]
+};
