f931755f60d5c4e5735ca308b570b0145f754294
tremia1 <74683189+tremia1@users.noreply.github.com>
2023-03-29 17:04:53 -0400

    Add files via upload

--- src/scenes/test.js
+++ src/scenes/test.js
@@ -1,1 +1,19 @@
+class test extends Phaser.Scene{
+    constructor(){
+        super("testGame");
+    }
+    preload(){
 
+        this.load.image("background","assets/background.png")
+    }
+
+    create(){
+
+        this.background = this.add.tileSprite(0,0,config.width,config.height,"background");
+        this.background.setOrigin(0,0);
+    }
+
+
+
+
+}
