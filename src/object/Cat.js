ae893a8420f26e682fc74dd54606897f4e0e752a
wweng2 <99202437+wweng2@users.noreply.github.com>
2023-04-04 16:06:24 -0400

    Update Cat.js
    Delete climb since we are getting rid of it and put the jump  in the else statement asking for it and not a separate functions and make it able to be export

--- src/object/Cat.js
+++ src/object/Cat.js
@@ -1,103 +1,79 @@
-class Cat extends Phaser.GameObjects.Sprite {
+export default class Cat extends Phaser.GameObjects.Sprite {
 
-    constructor(scene, x, y) {
-        super(scene, x, y, 'Cat');
+    constructor (scene){
+        super(scene, x,y,"Cat");
         this.scene.add.existing(this);
-        this.getBody().setCollideWorldBounds(true);
-        cursors = this.input.keyboard.createCursorKeys();
+        this.setCollideWorldBounds(true);
+        this.cursors = this.input.keyboard.createCursorKeys();
         this.setPosition(x, y);
     }
 
-    create() {
-
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('Cat', {
-                start: 0,
-                end: 3
-            }),
-            frameRate: 10,
-            repeat: -1
+    create()
+    {
+      
+      this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('Cat', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
         });
 
 
-        this.anims.create({
-            key: 'jump',
-            frames: this.anims.generateFrameNumbers('Cat', {
-                start: 5,
-                end: 5
-            }),
-            frameRate: 10,
-            repeat: -1
+      this.anims.create({
+      key: 'jump',
+      frames: this.anims.generateFrameNumbers('Cat', { start: 5, end: 5 }),
+      frameRate: 10,
+      repeat: -1
         });
 
-        this.anims.create({
-            key: 'climb',
-            frames: this.anims.generateFrameNumbers('Cat', {
-                start: 5,
-                end: 5
-            }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [{
-                key: 'Cat',
-                frame: 4
-            }],
-            frameRate: 20
+      
+      this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'Cat', frame: 4 } ],
+      frameRate: 20
         });
 
 
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', {
-                start: 5,
-                end: 8
-            }),
-            frameRate: 10,
-            repeat: -1
-        });
+      
+      this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('Cat', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+          });
     }
-    Update(time, delta) {
+    Update (time, delta)
+    {
+      
+      if (this.cursors.left.isDown)
+      {
+      this.setVelocityX(-160);
 
-        if (cursors.left.isDown) {
-            this.setVelocityX(-160);
+      this.anims.play('left', true);
+    }
+      else if (this.cursors.right.isDown)
+    {
+      this.setVelocityX(160);
 
-            this.anims.play('left', true);
-        } else if (cursors.right.isDown) {
-            this.setVelocityX(160);
+      this.anims.play('right', true);
+    }
+    else
+    {
+      this.setVelocityX(0);
 
-            this.anims.play('right', true);
-        } else {
-            this.setVelocityX(0);
+      this.anims.play('turn',true);
+    }
 
-            this.anims.play('turn');
-        }
-
-        if (cursors.up.isDown && this.body.touching.down) {
-            this.HighJump();
-        }
-
-        if (cursors.up.isDown && this.body.touching.left || cursors.up.isDown && this.body.touching.right) {
-            this.Climb();
-        }
-
+    if (this.cursors.up.isDown && this.body.touching.down)
+    {
+      this.setVeolcityY(-320
+      this.anims.play('jump', true);
     }
 
 
-    HighJump() {
-
-        this.setVeolcityY(-400)
-        this.anims.play('jump', true);
+      
     }
 
-    Climb {
-        this.setVeolcityY(-200)
-        this.anims.play('climb', true);
-    }
 
+    
 }
