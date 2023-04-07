dae039e780d63e3dddb49e20d517f04659ab422f
Michael Parchment <michaelparchmnt@gmail.com>
2023-04-07 17:46:54 -0400

    Fix serving.

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const app = express();
 const dbPath = './src/database/game.db';
 const db = new sqlite3.Database(dbPath);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/api', apiRouter);
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(__dirname + '/index.html');
 });
+
+app.use('/api', apiRouter);
 
 const port = 3000;
 app.listen(port, () => {
