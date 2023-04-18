e4878aa36f68d2d962cbd95c5c76e8bd602e60a0
tremia1 <tremia1@umbc.edu>
2023-04-17 21:55:43 -0400

    Merged maptest branch with origin main. Fixed errors

--- src/game.js
+++ src/game.js
@@ -18,6 +18,8 @@ class gameScene extends Phaser.Scene {
         this.load.spritesheet('DogBark', 'assets/images/Dog/Dog-Bark.png', { frameWidth: 22, frameHeight: 22 });
         this.load.spritesheet('DogSniffAndWalk', 'assets/images/Dog/Dog-Sniff-Walk.png', { frameWidth: 57, frameHeight: 61 });
 
+        this.load.spritesheet('Cat', 'assets/iamges/Cat.png', {frameWidth: 50, frameHeight: 40});
+
     }
     create() {
  
@@ -47,10 +49,7 @@ class gameScene extends Phaser.Scene {
         //    collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
         //    faceColor: new Phaser.Display.Color(40, 39, 37, 255)
         //});
-     
-
-          //   Create a new sprite for the player
-        this.player = this.physics.add.sprite(800, 600, 'player');
+    
 
         // Set up player input controls
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -61,20 +60,40 @@ class gameScene extends Phaser.Scene {
         });
 
 
-      
-
-        this.platforms = this.physics.add.staticGroup();
-        this.platforms.create(800,900, 'platform').setScale(3).refreshBody();
         //Create dog class
+        this.Dog = new Dog({
+            scene: this,
+            x: 800,
+            y: 500
+        });
         
         this.Cat = new Cat({
             scene: this,
             x: 800,
             y: 500
         });
+    //   //Create groups for button, block and water
+    //   this.buttonGroup = this.add.group();
+    //   this.blockGroup = this.add.group();
+    //   this.waterGroup = this.add.group();
+
+    //   //Create button class and set it as tilemap object
+    //   map.getObjectLayer('Button').objects.forEach((button) => {
+    //       const buttonObject = new Button({
+    //           scene: this,
+    //           x: button.x,
+    //           y: button.y,
+    //           status: false,
+    //           cat: this.cat,
+    //           dog: this.dog
+    //       });
+    //       this.buttonGroup.add(buttonObject);
+    //   });
+ 
 
        // this.physics.add.collider(this.player, platforms);
-       this.physics.add.collider(this.Cat, this.platforms);
+    //    this.physics.add.collider(this.Cat, this.platforms);
+    //    this.physics.add.collider(this.Dog, this.)
  
 
 
@@ -82,88 +101,17 @@ class gameScene extends Phaser.Scene {
     update() {
         console.log(this.cursors.up.isDown);
         this.Cat.update(this.cursors);
+        this.Dog.update(this.keys);
+//  //Update button group
+//  this.buttonGroup.children.entries.forEach((sprite) => {
+//     sprite.update();
+// });
+    }
         
-
         
-        
-          // Check for player input and move the player accordingly
-   
-        
-       
-       /* console.log(this.player.body.touching.down); //currently false when it should be true
-        // Set up the player's physics properties
-     
-        this.player.setCollideWorldBounds(true);
-        this.player.setBounce(0.2);
-        this.player.setGravityY(300);
-      
-
-        //Create dog class
-        this.dog = new Dog({
-            scene: this,
-            x: 800,
-            y: 600
-        });
-
-        //Create cat class
-        this.cat = new Cat({
-            scene: this,
-            x: 800,
-            y: 600
-        });
-
-        //Create groups for button, block and water
-        this.buttonGroup = this.add.group();
-        this.blockGroup = this.add.group();
-        this.waterGroup = this.add.group();
-
-        //Create button class and set it as tilemap object
-        map.getObjectLayer('Button').objects.forEach((button) => {
-            const buttonObject = new Button({
-                scene: this,
-                x: button.x,
-                y: button.y,
-                status: false,
-                cat: this.cat,
-                dog: this.dog
-            });
-            this.buttonGroup.add(buttonObject);
-        });
-   
-
-   
-
-        this.physics.add.collider(this.player, platforms);
-        this.physics.add.collider(this.dog, platforms);
-        this.physics.add.collider(this.cat, platforms);
-
        
 
 
-    }
-    update() {
-          // Check for player input and move the player accordingly
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-160);
-        } else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(160);
-        } else {
-            this.player.setVelocityX(0);
-        }
-    this.dog.update(this.keys);
 
-    //Update button group
-        this.buttonGroup.children.entries.forEach((sprite) => {
-            sprite.update();
-        });
-     
-        // Make the player jump if they're touching the ground
-        if (this.cursors.up.isDown && this.player.body.blocked) {
-            this.player.setVelocityY(-450);
-        }*/
-        }
-
-
-
-    }
+  
 } export default gameScene;
