57d9d81b400d6a72870efbf15b3822eb8566c771
Wei Zhang Weng <wweng2@umbc.edu>
2023-04-19 16:28:28 -0400

    Make the startmenu

---
+++ src/scenes/StartMenu.js
@@ -0,0 +1,39 @@
+
+class StartMenu extends Phaser.Scene {
+
+    constructor()
+	{
+		super('StartMenu')
+	}
+    preload() {
+        this.load.image('background', 'assets/images/background.png');
+
+        this.load.image('cursor','assets/images/cursor.png');
+        this.load.image('wood','assets/images/wood.png');
+
+
+    }
+    create() {
+ 
+  
+
+
+
+ 
+
+ 
+ 
+
+
+    }
+    update() {
+
+    }
+        
+        
+       
+
+
+
+  
+} export default StartMenu;
